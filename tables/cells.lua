578947c7d1cc8733d6ccc252d72eb4352eaf9452
Antonio Vieiro <antonio@antonioshome.net>
2014-01-19 14:33:14 +0100

    Different enhancements as per Philipp suggestions

--- tables/cells.lua
+++ tables/cells.lua
@@ -7,18 +7,26 @@
 
 local M = {}
 
-M.is_atom = function (c)
-  return c ~= nil and c._t == nil
-end
+-- Index '1' is used for car
+-- Index '2' is used for cdr
+-- Index '3' is used for type (to distinguish between cells and other tables)
 
+-- Returns true if c is nil
 M.is_nil = function (c)
   return c == nil
 end
 
+-- Returns true if c is a cell (a table with [3] == 'cell')
 M.is_cons = function (c)
-  return c ~= nil and c._t == 'cell'
+  return c ~= nil and type(c) == 'table' and c[3] == 'cell'
 end
 
+-- Returns true if c is an atom (an object not nil and not a cell)
+M.is_atom = function (c)
+  return not M.is_nil(c) and not M.is_cons(c)
+end
+
+-- Quick & dirty print function, without cycles
 M.tostring = function (c)
   if M.is_nil(c) then
     return '()'
@@ -27,8 +35,8 @@ M.tostring = function (c)
   else
     local s = '('
     while true do
-       s = s .. M.tostring(c._car)
-       c = c._cdr
+       s = s .. M.tostring(c[1])
+       c = c[2]
        if M.is_nil(c) then
          s = s .. ')'
          break
@@ -43,59 +51,66 @@ M.tostring = function (c)
   end
 end
 
-M.car    = function (c) return c._car end
-M.cdr    = function (c) return c._cdr end
-M.cadr   = function (c) return c._cdr._car end
-M.cddr   = function (c) return c._cdr._cdr end
-M.caddr  = function (c) return c._cdr._cdr._car end
-M.cdddr  = function (c) return c._cdr._cdr._cdr end
-M.cadddr = function (c) return c._cdr._cdr._cdr._car end
-M.cddddr = function (c) return c._cdr._cdr._cdr._cdr end
+-- Miscellaneous functions
+M.car    = function (c) return c[1] end
+M.cdr    = function (c) return c[2] end
+M.cadr   = function (c) return c[2][1] end
+M.cddr   = function (c) return c[2][2] end
+M.caddr  = function (c) return c[2][2][1] end
+M.cdddr  = function (c) return c[2][2][2] end
+M.cadddr = function (c) return c[2][2][2][1] end
+M.cddddr = function (c) return c[2][2][2][2] end
+M.set_car = function (c, car) c[1] = car end
+M.set_cdr = function (c, cdr) c[2] = cdr end
 
+-- Returns last in cell
 M.last = function (c)
   while true do
-    if c._cdr == nil then
-      return c._car
+    if c[2] == nil then
+      return c[1]
     end
-    c = c._cdr
+    c = c[2]
   end
 end
 
+-- Returns length
 M.length = function (c)
   local len = 0
   while c ~= nil do
     len = len + 1
-    c = c._cdr
+    c = c[2]
   end
   return len
 end
 
+-- Returns nth
 M.nth = function (c, n)
   while n > 1 do
     n = n - 1
-    c = c._cdr
+    c = c[2]
   end
-  return c._car
+  return c[1]
 end
 
+-- Appends two cons cells
 M.append = function (a, b)
   local clone_conses = function (l)
     if l == nil then
       return nil, nil
     else
-      local first = M.cons(a._car, nil)
+      local first = M.cons(a[1], nil)
       local previous = first
-      l = l._cdr
+      l = l[2]
       while true do
         if l == nil then
           last = previous
-          previous._cdr = nil
+          previous[2] = nil
           return first, last
         else
-          local c = M.cons(l._car, nil)
-          previous._cdr = c
+          local c = M.cons(l[1], nil)
+          previous[2] = c
           previous = c
-          l = l._cdr
+          l = l[2]
         end
       end
     end
@@ -104,24 +119,17 @@ M.append = function (a, b)
     return b
   else
     local clone, last = clone_conses(a)
-    last._cdr = a == nil and nil or b
+    last[2] = a == nil and nil or b
     return clone
   end
 end
 
-local cell_metatable = {
-  __tostring = M.tostring,
-}
-
-M.cons = function (...)
-  local arg = {...}
-  local c = setmetatable({
-      _t        = 'cell',
-      _car      = arg[1],
-      _cdr      = arg[2],
+-- fallback __index table
+local cell_methods = {
       is_cons   = M.is_cons,
       is_nil    = M.is_nil,
       is_atom   = M.is_atom,
+
       car       = M.car,
       cdr       = M.cdr,
       cadr      = M.cadr,
@@ -130,11 +138,30 @@ M.cons = function (...)
       cdddr     = M.cdddr,
       cadddr    = M.cadddr,
       cddddr    = M.cddddr,
+
       last      = M.last,
       length    = M.length,
       nth       = M.nth,
-      }, cell_metatable);
-  return c
+
+      set_car   = M.set_car,
+      set_cdr   = M.set_cdr,
+}
+
+local cell_metatable = {
+  __tostring = M.tostring,
+  __index    = cell_methods,
+}
+
+
+-- Creates a new cell with (car, cdr)
+M.cons = function (car, cdr)
+  if car == nil and cdr == nil then return nil end
+  local cons_cell = {
+    [1] = car,
+    [2] = cdr,
+    [3] = 'cell'
+  }
+  return setmetatable(cons_cell, cell_metatable);
 end
 
 return M
--- tables/test.lua
+++ tables/test.lua
@@ -7,12 +7,15 @@
 
 local cells = require 'cells'
 
-local cons   = cells.cons
-local car    = cells.car
-local cdr    = cells.cdr
-local append = cells.append
-local last   = cells.last
-local length = cells.length
+local cons    = cells.cons
+local car     = cells.car
+local cdr     = cells.cdr
+local append  = cells.append
+local last    = cells.last
+local length  = cells.length
+local is_atom = cells.is_atom
+local is_cons = cells.is_cons
+local is_nil  = cells.is_nil
 
 local a = cons("A")
 local b = cons(cons("A"), cons("B"))
@@ -21,19 +24,31 @@ local c = cons("A", cons("B"))
 local v = {a,b,c}
 
 for _, a in ipairs(v) do
-  print("SEXPR   : ", a)
-  print("is_cons : ", a:is_cons())
-  print("is_nil  : ", a:is_nil())
-  print("car     : ", car(a))
-  print("cdr     : ", cdr(a))
-  print(":car    : ", a:car())
-  print(":cdr    : ", a:cdr())
-  print("last    : ", a:last())
-  print("length  : ", a:length())
-  print(":nth(1) : ", a:nth(1))
+  print("SEXPR      : ", a)
+  print("is_cons    : ", a:is_cons())
+  print("is_nil     : ", a:is_nil())
+  print("is_atom    : ", a:is_atom())
+
+  print("car        : ", car(a))
+  print(":car       : ", a:car())
+  print("car is_atom: ", is_atom(car(a)))
+
+  print("cdr        : ", cdr(a))
+  print(":cdr       : ", a:cdr())
+  print("cdr is_atom: ", is_atom(cdr(a)))
+
+  print("last       : ", a:last())
+  print("length     : ", a:length())
+  print(":nth(1)    : ", a:nth(1))
   print()
 end
 
 print("B            = ", b)
 print("C            = ", c)
 print("(append B C) = ", append(b, c))
+a:set_car(a)
+
+a = nil
+b = nil
+c = nil
+
